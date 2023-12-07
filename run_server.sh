3de202912df8513fefa094c9bc6528e4fd98ab16
MayGrass <15877196+MayGrass@users.noreply.github.com>
2023-12-07 21:07:52 +0800

    [FEAT] docker compose deploy add test step

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,13 @@
 # wait mysql
 while ! nc -z $DB_HOST $DB_PORT; do sleep 3; done
 
+# run test
+python manage.py test
+if [ $? -ne 0 ]; then
+    echo "Test failed"
+    exit 1
+fi
+
 # update database
 python manage.py migrate
 
